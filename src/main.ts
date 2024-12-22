556c498cebd1ebbc79a986a7f7888c9451b6d6ed
haidar180202 <gshaidar6@gmail.com>
2024-12-22 14:04:06 +0700

    refactor2: update src/main.ts for improved application initialization
    - Modified: src/main.ts
    - Description: Adjusted application bootstrap logic for better readability, maintainability, or to include new middleware, services, or configurations.

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen("https://menu-management-backend-weld.vercel.app/");
+  await app.listen("https://menu-management-backend-weld.vercel.app");
 }
 bootstrap();
